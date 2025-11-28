54b8c128cbb6356158930ab47db88807b61fd16a
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2025-11-28 23:39:08 +0100

    Update Kernel.cu

--- Kernel.cu
+++ Kernel.cu
@@ -4,4 +4,18 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
+__global__ void naive_butterfly_xw_kernel(
+    float *X, float *W, float *Y,
+    float *B, float *F, float *L, float *e)
+{
+
+    int bf = blockIdx.x;    
+
+    int b = bf / F;         
+    int f = bf % F;         
+
+    int tid = threadIdx.x;  
+}
+
+
 
