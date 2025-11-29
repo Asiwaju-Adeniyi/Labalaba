4aeed6ca9ae72a1c5a4ffefe396d8258869aced7
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2025-11-29 23:18:06 +0100

    Update Kernel.cu

--- Kernel.cu
+++ Kernel.cu
@@ -6,16 +6,19 @@
 
 __global__ void naive_butterfly_xw_kernel(
     float *X, float *W, float *Y,
-    float *B, float *F, float *L, float *e)
+    int B, int F, int L, int e)
 {
+    // Which row we’re working on
+    int bf = blockIdx.x;
+    int b = bf / F;
+    int f = bf % F;
 
-    int bf = blockIdx.x;    
+    int tid = threadIdx.x;  // lane inside the row
 
-    int b = bf / F;         
-    int f = bf % F;         
+    // Pointer to the beginning of this (b,f) row
+    int row_offset = (b * F + f) * L;
+    float* x_row = X + row_offset;
+    float* y_row = Y + row_offset; 
 
-    int tid = threadIdx.x;  
 }
-
-
 
